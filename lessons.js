c622723a7bbd02235a85336c01b0bf0ebd904a60
gurthebest <91569638+gurthebest@users.noreply.github.com>
2021-11-19 12:54:30 +0000

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -14,7 +14,7 @@
             "subject": "English",
             "location": "London",
             "price": 299,
-            "image": "img/math.png",
+            "image": "img/eng.png",
             "availableInventory": 5,
     
         },
@@ -23,7 +23,7 @@
             "subject": "Art",
             "location": "Birmingham",
             "price": 199, 
-            "image": "img/math.png",
+            "image": "img/art.png",
             "availableInventory": 5,
     
         },
@@ -32,7 +32,7 @@
             "subject": "Music",
             "location": "Birmingham",
             "price": 299,
-            "image": "img/math.png",
+            "image": "img/music.png",
             "availableInventory": 5,
     
         },
@@ -41,7 +41,7 @@
             "subject": "Physical education",
             "location": "London",
             "price": 499,
-            "image": "img/math.png",
+            "image": "img/physicaleducation.png",
             "availableInventory": 5,
     
         },
@@ -50,7 +50,7 @@
             "subject": "Science",
             "location": "London",
             "price": 99.00,
-            "image": "img/math.png",
+            "image": "img/science.png",
             "availableInventory": 5,
     
         },
@@ -59,7 +59,7 @@
             "subject": "Spanish",
             "location": "Birmingham",
             "price": 699.00,
-            "image": "img/math.png",
+            "image": "img/spanish.png",
             "availableInventory": 5,
     
         },
@@ -68,7 +68,7 @@
             "subject": "French",
             "location": "Birmingham",
             "price": 89.00,
-            "image": "img/math.png",
+            "image": "img/french.png",
             "availableInventory": 5,
     
         },
@@ -77,7 +77,7 @@
             "subject": "English additional",
             "location": "Birmingham",
             "price": 199.00,
-            "image": "img/math.png",
+            "image": "img/engadd.png",
             "availableInventory": 5,
     
         },
@@ -86,7 +86,7 @@
             "subject": "Italian",
             "location": "Birmingham",
             "price": 199.00,
-            "image": "img/math.png",
+            "image": "img/italy.png",
             "availableInventory": 5,
     
         },
