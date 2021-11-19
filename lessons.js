9691c4bab4a56b9c3f500914f22f6c3f55d621ef
gurthebest <91569638+gurthebest@users.noreply.github.com>
2021-11-19 12:20:47 +0000

    Add files via upload
    This is the lessons array used

---
+++ lessons.js
@@ -0,0 +1,93 @@
+
+    lessons= [
+        {
+            "id": 201,
+            "subject": "Math",
+            "location": "London",
+            "price":  199 ,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 202,
+            "subject": "English",
+            "location": "London",
+            "price": 299,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 203,
+            "subject": "Art",
+            "location": "Birmingham",
+            "price": 199, 
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 204,
+            "subject": "Music",
+            "location": "Birmingham",
+            "price": 299,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 205,
+            "subject": "Physical education",
+            "location": "London",
+            "price": 499,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 206,
+            "subject": "Science",
+            "location": "London",
+            "price": 99.00,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 207,
+            "subject": "Spanish",
+            "location": "Birmingham",
+            "price": 699.00,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 208,
+            "subject": "French",
+            "location": "Birmingham",
+            "price": 89.00,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 209,
+            "subject": "English additional",
+            "location": "Birmingham",
+            "price": 199.00,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+        {
+            "id": 210,
+            "subject": "Italian",
+            "location": "Birmingham",
+            "price": 199.00,
+            "image": "img/math.png",
+            "availableInventory": 5,
+    
+        },
+    ]
